ddb68ae7a2dfb71b9234115286c5c9d00e1ec7f0
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-09 10:46:36 +0300

    Move `URLSessionHTTPClient` to its own file

---
+++ Simonsvoss/URLSessionHTTPClient.swift
@@ -0,0 +1,32 @@
+//
+//  URLSessionHTTPClient.swift
+//  Simonsvoss
+//
+//  Created by Mohammad Bitar on 7/9/22.
+//
+
+import Foundation
+
+typealias HTTPClientResult = Result<(Data, HTTPURLResponse), Error>
+
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success((data, response)))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
--- SimonsvossTests/URLSessionHTTPClientTests.swift
+++ SimonsvossTests/URLSessionHTTPClientTests.swift
@@ -1,30 +1,6 @@
 @testable import Simonsvoss
 import XCTest
 
-typealias HTTPClientResult = Result<(Data, HTTPURLResponse), Error>
-
-class URLSessionHTTPClient {
-    private let session: URLSession
-    
-    private struct UnexpectedValuesRepresentation: Error {}
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success((data, response)))
-            } else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
-        }.resume()
-    }
-}
-
 final class URLSessionHTTPClientTests: XCTestCase {
     
     override func setUp() {

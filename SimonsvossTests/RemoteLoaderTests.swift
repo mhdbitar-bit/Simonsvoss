ccbda6857c1d11cc79e4efb380d500dcbc9a53c9
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-09 12:15:33 +0300

    Test request data from url

--- SimonsvossTests/RemoteLoaderTests.swift
+++ SimonsvossTests/RemoteLoaderTests.swift
@@ -1,12 +1,38 @@
 @testable import Simonsvoss
 import XCTest
 
+final class RemoteLoader {
+    private let url: URL
+    private let client: HTTPClient
+
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    func load(completion: @escaping (Result<Item, Error>) -> Void) {
+        client.get(from: url) { result in
+            completion(.failure(NSError()))
+        }
+    }
+}
+
 final class RemoteLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_load_requestsDataFromURL() {
+        let url = anyURL()
+        let client = HTTPClientSpy()
+        let sut = RemoteLoader(url: url, client: client)
+        
+        sut.load { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     
     // MARK: - Helpers
@@ -19,7 +45,7 @@ final class RemoteLoaderTests: XCTestCase {
         }
         
         func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-     
+            messages.append((url, completion))
         }
     }
 }

ae2390a5d6da68d974d084c88d238823064084e8
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-09 10:00:07 +0300

    Add Presentable Item

---
+++ Simonsvoss/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Simonsvoss
+//
+//  Created by Mohammad Bitar on 7/9/22.
+//
+
+import Foundation
+
+
+struct Item {
+    let lockName: String
+    let buildingShortcut: String
+    let floor: String
+    let roomNumber: String
+}

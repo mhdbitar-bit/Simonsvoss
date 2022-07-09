9acff04e7f04d081b383601eccf6f43734a02040
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-09 14:41:32 +0300

    Move `HTTPClientSpy` to its own file

--- SimonsvossTests/RemoteLoaderTests.swift
+++ SimonsvossTests/RemoteLoaderTests.swift
@@ -383,31 +383,7 @@ final class RemoteLoaderTests: XCTestCase {
         return (item, json)
     }
     
-    class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
-        
-        var requestedURLs: [URL] {
-            messages.map { $0.url }
-        }
-        
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-            messages.append((url, completion))
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
-            let response = HTTPURLResponse(
-                url: requestedURLs[index],
-                statusCode: code,
-                httpVersion: nil,
-                headerFields: nil
-            )!
-            messages[index].completion(.success((data, response)))
-        }
-    }
+    
     
     private func makeItemsJSON(_ items: [String: Any]) -> Data {
         return try! JSONSerialization.data(withJSONObject: items)

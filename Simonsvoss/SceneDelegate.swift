7114d670d7efc270608d7a88bce2a3178c0dfc44
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-10 18:44:09 +0300

    Compose loader with view model and view controller

--- Simonsvoss/SceneDelegate.swift
+++ Simonsvoss/SceneDelegate.swift
@@ -10,8 +10,28 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    private let navigationController = UINavigationController()
+    let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        configureWindow()
+    }
+    
+    func configureWindow() {
+        navigationController.setViewControllers([makeRootViewController()], animated: false)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+    
+    private func makeRootViewController() -> ListViewController {
+        let baseURL = URL(string: "https://simonsvoss-homework.herokuapp.com/sv_lsm_data.json")!
+        let loader = RemoteLoader(url: baseURL, client: remoteClient)
+        let viewModel = ListViewModel(loader: MainQueueDispatchDecorator(decoratee: loader))
+        let viewController = ListViewController(viewModel: viewModel)
+        return viewController
     }
 }

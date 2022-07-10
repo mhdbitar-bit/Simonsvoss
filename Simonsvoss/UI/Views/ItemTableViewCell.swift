d47cd2635b557eae020f62e410d527546cde051f
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-10 07:50:22 +0300

    Add `ItemTableViewCell`

---
+++ Simonsvoss/UI/Views/ItemTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemTableViewCell.swift
+//  Simonsvoss
+//
+//  Created by Mohammad Bitar on 7/10/22.
+//
+
+import UIKit
+
+final class ItemTableViewCell: UITableViewCell {
+    @IBOutlet var lockNameLabel: UILabel!
+    @IBOutlet var metaLabel: UILabel!
+}

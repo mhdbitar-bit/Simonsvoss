503c8f8c4c4165db012a59efc24b1d250f30aa86
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-10 07:55:14 +0300

    Add `ItemViewModel`

---
+++ Simonsvoss/UI/ViewModels/ItemViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ItemViewModel.swift
+//  Simonsvoss
+//
+//  Created by Mohammad Bitar on 7/10/22.
+//
+
+import Foundation
+
+struct ItemViewModel: Equatable {
+    let lockName: String
+    let buildingShortcut: String
+    let floor: String
+    let roomNumber: String
+}
